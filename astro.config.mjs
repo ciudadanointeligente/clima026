76d4759216100f10124530c871791bc4d5e0f40a
pedregalux <felipe.perry@vivaldi.net>
2026-04-30 15:34:27 -0400

    init

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import markdoc from '@astrojs/markdoc';
+import keystatic from '@keystatic/astro';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import alpinejs from '@astrojs/alpinejs';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), markdoc(), keystatic(), alpinejs()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
